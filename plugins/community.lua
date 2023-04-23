eefc037e295251179b830ae9b4cc0104c078012c
Tony Scinocco <scinocco.a@gmail.com>
2023-04-23 13:49:05 -0400

    base config

--- plugins/community.lua
+++ plugins/community.lua
@@ -6,4 +6,13 @@ return {
 
   -- { import = "astrocommunity.colorscheme.catppuccin" },
   -- { import = "astrocommunity.completion.copilot-lua-cmp" },
+  { import = "astrocommunity.pack.rust" },
+  { import = "astrocommunity.pack.typescript" },
+  { import = "astrocommunity.pack.tailwindcss" },
+  { import = "astrocommunity.pack.go" },
+  { import = "astrocommunity.pack.yaml" },
+  { import = "astrocommunity.pack.markdown" },
+  { import = "astrocommunity.pack.json" },
+  { import = "astrocommunity.pack.python" },
+  { import = "astrocommunity.colorscheme.nightfox" },
 }
