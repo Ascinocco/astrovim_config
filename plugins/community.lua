cfccb63b942904f7c1ba343ca947f13e7d9596b4
Tony Scinocco <tony.scinocco@missionlane.com>
2023-04-26 10:29:59 -0400

    add git blame

--- plugins/community.lua
+++ plugins/community.lua
@@ -15,4 +15,5 @@ return {
   { import = "astrocommunity.pack.json" },
   { import = "astrocommunity.pack.python" },
   { import = "astrocommunity.colorscheme.nightfox" },
+  { import = "astrocommunity.git.git-blame-nvim" },
 }
